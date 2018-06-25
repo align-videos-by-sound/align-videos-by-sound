a3e479ec27ec1b1e5b921c9b5a02c421b604b944
hhsprings <xwhhsprings@gmail.com>
2018-06-25 22:31:04 +0900

    * Following the standard Python's manner document style.
    Do test that:
    [me@host: align_videos_by_soundtrack]$ python setup.py install
    [me@host: align_videos_by_soundtrack]$ python -m pydoc align_videos_by_soundtrack.align | less
    [me@host: align_videos_by_soundtrack]$ alignment_info_by_sound_track -h
    [me@host: align_videos_by_soundtrack]$ alignment_info_by_sound_track \
    > tests/testfiles/3-secs-in.mp4 tests/testfiles/full-with-prepad-sec-180.mp4 --j
    [me@host: align_videos_by_soundtrack]$ alignment_info_by_sound_track \
    > tests/testfiles/3-secs-in.mp4 tests/testfiles/no_such_movie.mp4
    
    * Hide private methods.
    * __all__
    * Give args[1:] to parser.parse_args

--- align_videos_by_soundtrack/align.py
+++ align_videos_by_soundtrack/align.py
@@ -3,9 +3,15 @@
 #
 # This script based on alignment_by_row_channels.py by Allison Deal, see
 # https://github.com/allisonnicoledeal/VideoSync/blob/master/alignment_by_row_channels.py
+"""
+This module contains the detector class for knowing the offset
+difference for audio and video files, containing audio recordings
+from the same event. It relies on ffmpeg being installed and
+the python libraries scipy and numpy.
+"""
 from __future__ import unicode_literals
 
-DOC = '''
+_doc_template = '''
 This program reports the offset difference for audio and video files,
 containing audio recordings from the same event. It relies on ffmpeg being
 installed and the python libraries scipy and numpy.
@@ -13,15 +19,15 @@ installed and the python libraries scipy and numpy.
 
 Usage:
 
-    %s <file1> <file2>
+    %(prog)s <file1> <file2>
 
 It reports back the offset. Example:
 
-    %s good_video_shitty_audio.mp4 good_audio_shitty_video.mp4
+    %(prog)s good_video_shitty_audio.mp4 good_audio_shitty_video.mp4
 
     Result: The beginning of good_video_shitty_audio.mp4 needs to be trimmed off 11.348 seconds for all files to be in sync
 
-''' % (__file__, __file__)
+'''
 import os
 import sys
 from collections import defaultdict
@@ -33,6 +39,11 @@ import logging
 import numpy as np
 import scipy.io.wavfile
 
+
+__all__ = [
+    'SyncDetector',
+    'main',
+    ]
 
 _logger = logging.getLogger(__name__)
 
@@ -51,22 +62,25 @@ else:
         return s
 
 
-# Read file
-# INPUT: Audio file
-# OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
-def read_audio(audio_file):
+def _read_audio(audio_file):
+    """
+    Read file
+
+    INPUT: Audio file
+    OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
+    """
     rate, data = scipy.io.wavfile.read(audio_file)  # Return the sample rate (in samples/sec) and data from a WAV file
     return data, rate
 
 
-def make_horiz_bins(data, fft_bin_size, overlap, box_height):
+def _make_horiz_bins(data, fft_bin_size, overlap, box_height):
     horiz_bins = defaultdict(list)
 
     # process sample and set matrix height
     for x, j in enumerate(range(int(-overlap), len(data), int(fft_bin_size - overlap))):
         sample_data = data[max(0, j):max(0, j) + fft_bin_size]
         if (len(sample_data) == fft_bin_size):  # if there are enough audio points left to create a full fft bin
-            intensities = fourier(sample_data)  # intensities is list of fft results
+            intensities = _fourier(sample_data)  # intensities is list of fft results
             for i in range(len(intensities)):
                 box_y = i // box_height
                 horiz_bins[box_y].append((intensities[i], x, i))  # (intensity, x, y)
@@ -74,16 +88,19 @@ def make_horiz_bins(data, fft_bin_size, overlap, box_height):
     return horiz_bins
 
 
-# Compute the one-dimensional discrete Fourier Transform
-# INPUT: list with length of number of samples per second
-# OUTPUT: list of real values len of num samples per second
-def fourier(sample):  # , overlap):
+def _fourier(sample):  # , overlap):
+    """
+    Compute the one-dimensional discrete Fourier Transform
+
+    INPUT: list with length of number of samples per second
+    OUTPUT: list of real values len of num samples per second
+    """
     fft_data = np.fft.fft(sample)  # Returns real and complex value pairs
     fft_data = fft_data[:len(fft_data) // 2]
     return list(np.sqrt(fft_data.real**2 + fft_data.imag**2))
 
 
-def make_vert_bins(horiz_bins, box_width):
+def _make_vert_bins(horiz_bins, box_width):
     boxes = defaultdict(list)
     for key in list(horiz_bins.keys()):
         for i in range(len(horiz_bins[key])):
@@ -93,7 +110,7 @@ def make_vert_bins(horiz_bins, box_width):
     return boxes
 
 
-def find_bin_max(boxes, maxes_per_box):
+def _find_bin_max(boxes, maxes_per_box):
     freqs_dict = defaultdict(list)
     for key in list(boxes.keys()):
         max_intensities = sorted(boxes[key], key=lambda x: -x[0])[:maxes_per_box]
@@ -103,14 +120,14 @@ def find_bin_max(boxes, maxes_per_box):
     return freqs_dict
 
 
-def find_freq_pairs(freqs_dict_orig, freqs_dict_sample):
+def _find_freq_pairs(freqs_dict_orig, freqs_dict_sample):
     for key in set(freqs_dict_sample.keys()) & set(freqs_dict_orig.keys()):
         for iitem in freqs_dict_sample[key]:  # determine time offset
             for jitem in freqs_dict_orig[key]:
                 yield (iitem, jitem)
 
 
-def find_delay(time_pairs):
+def _find_delay(time_pairs):
     t_diffs = defaultdict(int)
     for pair in time_pairs:
         delta_t = pair[0] - pair[1]
@@ -138,10 +155,13 @@ class SyncDetector(object):
     def __exit__(self, type, value, tb):
         shutil.rmtree(self._working_dir)
 
-    # Extract audio from video file, save as wav auido file
-    # INPUT: Video file, and its index of input file list
-    # OUTPUT: Does not return any values, but saves audio as wav file
-    def extract_audio(self, video_file, idx):
+    def _extract_audio(self, video_file, idx):
+        """
+        Extract audio from video file, save as wav auido file
+
+        INPUT: Video file, and its index of input file list
+        OUTPUT: Does not return any values, but saves audio as wav file
+        """
         _ffmpeg_ss_args = (None, None)
         if idx in self._known_delay_ge_map:
             ss_h = self._known_delay_ge_map[idx] // 3600
@@ -171,36 +191,38 @@ class SyncDetector(object):
             subprocess.check_call(map(_encode, cmd), stderr=open(os.devnull, 'w'))
         return output
 
-    # Find time delay between two video files
     def align(self, files, fft_bin_size=1024, overlap=0, box_height=512, box_width=43, samples_per_box=7):
+        """
+        Find time delays between video files
+        """
         tmp_result = [0.0]
 
         # Process first file
-        wavfile1 = self.extract_audio(files[0], 0)
-        raw_audio1, rate = read_audio(wavfile1)
-        bins_dict1 = make_horiz_bins(
+        wavfile1 = self._extract_audio(files[0], 0)
+        raw_audio1, rate = _read_audio(wavfile1)
+        bins_dict1 = _make_horiz_bins(
             raw_audio1,
             fft_bin_size, overlap, box_height)  # bins, overlap, box height
         del raw_audio1
-        boxes1 = make_vert_bins(bins_dict1, box_width)  # box width
-        ft_dict1 = find_bin_max(boxes1, samples_per_box)  # samples per box
+        boxes1 = _make_vert_bins(bins_dict1, box_width)  # box width
+        ft_dict1 = _find_bin_max(boxes1, samples_per_box)  # samples per box
         del boxes1
 
         for i in range(len(files) - 1):
             # Process second file
-            wavfile2 = self.extract_audio(files[i + 1], i + 1)
-            raw_audio2, rate = read_audio(wavfile2)
-            bins_dict2 = make_horiz_bins(
+            wavfile2 = self._extract_audio(files[i + 1], i + 1)
+            raw_audio2, rate = _read_audio(wavfile2)
+            bins_dict2 = _make_horiz_bins(
                 raw_audio2,
                 fft_bin_size, overlap, box_height)
             del raw_audio2
-            boxes2 = make_vert_bins(bins_dict2, box_width)
-            ft_dict2 = find_bin_max(boxes2, samples_per_box)
+            boxes2 = _make_vert_bins(bins_dict2, box_width)
+            ft_dict2 = _find_bin_max(boxes2, samples_per_box)
             del boxes2
 
             # Determie time delay
-            pairs = find_freq_pairs(ft_dict1, ft_dict2)
-            delay = find_delay(pairs)
+            pairs = _find_freq_pairs(ft_dict1, ft_dict2)
+            delay = _find_delay(pairs)
             samples_per_sec = float(rate) / float(fft_bin_size)
             seconds = float(delay) / float(samples_per_sec)
 
@@ -227,8 +249,8 @@ class SyncDetector(object):
         #    }
 
 
-def bailout():
-    print(DOC)
+def _bailout(doc):
+    print(doc)
     sys.exit()
 
 
@@ -236,12 +258,13 @@ def main(args=sys.argv):
     import argparse
     import json
 
-    parser = argparse.ArgumentParser(description=DOC)
+    doc = _doc_template % ({"prog": args[0]})
+    parser = argparse.ArgumentParser(prog=args[0], usage=_doc_template)
     parser.add_argument('--max_misalignment', type=int, default=2*60)
     parser.add_argument('--known_delay_ge_map', type=str)
     parser.add_argument('--json', action="store_true",)
     parser.add_argument('file_names', nargs="*")
-    args = parser.parse_args()
+    args = parser.parse_args(args[1:])
     known_delay_ge_map = {}
     if args.known_delay_ge_map:
         known_delay_ge_map = json.loads(args.known_delay_ge_map)
@@ -256,14 +279,14 @@ def main(args=sys.argv):
         file_specs = list(map(_decode, map(os.path.abspath, args.file_names)))
         # _logger.debug(file_specs)
     else:  # No pipe and no input file, print help text and exit
-        bailout()
+        _bailout(doc)
     non_existing_files = []
     for path in file_specs:
         if not os.path.isfile(path):
             non_existing_files.append(path)
     if non_existing_files:
         print("** The following are not existing files: %s **" % (','.join(non_existing_files),))
-        bailout()
+        _bailout(doc)
 
     with SyncDetector(args.max_misalignment, known_delay_ge_map=known_delay_ge_map) as det:
         result = det.align(file_specs)
