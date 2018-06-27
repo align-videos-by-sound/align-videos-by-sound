9b0c69f9e42c3e300a8a419844c92867da3bdb4b
hhsprings <xwhhsprings@gmail.com>
2018-06-27 12:38:39 +0900

    Adding communicate.py., and adding "get_media_info".

--- align_videos_by_soundtrack/align.py
+++ align_videos_by_soundtrack/align.py
@@ -10,6 +10,7 @@ from the same event. It relies on ffmpeg being installed and
 the python libraries scipy and numpy.
 """
 from __future__ import unicode_literals
+from __future__ import absolute_import
 
 _doc_template = '''
     %(prog)s <file1> <file2>
@@ -28,14 +29,13 @@ It reports back the offset. Example:
 import os
 import sys
 from collections import defaultdict
-import subprocess
 import tempfile
 import shutil
 import logging
 
 import numpy as np
-import scipy.io.wavfile
 
+from . import communicate
 
 __all__ = [
     'SyncDetector',
@@ -46,28 +46,11 @@ _logger = logging.getLogger(__name__)
 
 
 if hasattr("", "decode"):  # python 2
-    def _encode(s):
-        return s.encode(sys.stdout.encoding)
-
     def _decode(s):
         return s.decode(sys.stdout.encoding)
 else:
-    def _encode(s):
-        return s
-
     def _decode(s):
         return s
-
-
-def _read_audio(audio_file):
-    """
-    Read file
-
-    INPUT: Audio file
-    OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
-    """
-    rate, data = scipy.io.wavfile.read(audio_file)  # Return the sample rate (in samples/sec) and data from a WAV file
-    return data, rate
 
 
 def _make_horiz_bins(data, fft_bin_size, overlap, box_height):
@@ -173,7 +156,7 @@ class SyncDetector(object):
         audio_output = track_name + "WAV.wav"  # !! CHECK TO SEE IF FILE IS IN UPLOADS DIRECTORY
         output = os.path.join(self._working_dir, audio_output)
         if not os.path.exists(output):
-            cmd = list(filter(None, [
+            cmd = [
                     "ffmpeg", "-y",
                     _ffmpeg_ss_args[0], _ffmpeg_ss_args[1],
                     self._ffmpeg_t_args[0], self._ffmpeg_t_args[1],
@@ -183,9 +166,9 @@ class SyncDetector(object):
                     "-ac", "1",
                     "-f", "wav",
                     "%s" % output
-                    ]))
+                    ]
             #_logger.debug(cmd)
-            subprocess.check_call(map(_encode, cmd), stderr=open(os.devnull, 'w'))
+            communicate.check_call(cmd, stderr=open(os.devnull, 'w'))
         return output
 
     def align(self, files, fft_bin_size=1024, overlap=0, box_height=512, box_width=43, samples_per_box=7):
@@ -196,7 +179,7 @@ class SyncDetector(object):
 
         # Process first file
         wavfile1 = self._extract_audio(files[0], 0)
-        raw_audio1, rate = _read_audio(wavfile1)
+        raw_audio1, rate = communicate.read_audio(wavfile1)
         bins_dict1 = _make_horiz_bins(
             raw_audio1,
             fft_bin_size, overlap, box_height)  # bins, overlap, box height
@@ -208,7 +191,7 @@ class SyncDetector(object):
         for i in range(len(files) - 1):
             # Process second file
             wavfile2 = self._extract_audio(files[i + 1], i + 1)
-            raw_audio2, rate = _read_audio(wavfile2)
+            raw_audio2, rate = communicate.read_audio(wavfile2)
             bins_dict2 = _make_horiz_bins(
                 raw_audio2,
                 fft_bin_size, overlap, box_height)
---
+++ align_videos_by_soundtrack/communicate.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+#
+"""
+This module contains helpers for cooperation processing with
+external programs (such as ffmpeg) on ​​which this library depends.
+"""
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import subprocess
+import sys
+import re
+import logging
+
+import scipy.io.wavfile
+
+__all__ = [
+    "check_call", "check_stderroutput",
+    "read_audio",
+    "get_media_info",
+    ]
+
+_logger = logging.getLogger(__name__)
+
+
+if hasattr("", "decode"):  # python 2
+    def _encode(s):
+        return s.encode(sys.stdout.encoding)
+else:
+    def _encode(s):
+        return s
+
+
+# ##################################
+#
+# low-level APIs
+#
+
+def _filter_args(*cmd):
+    """
+    do filtering None, and do encoding items to bytes
+    (in Python 2).
+    """
+    return map(_encode, filter(None, *cmd))
+
+    
+def check_call(*popenargs, **kwargs):
+    """
+    Basically do simply forward args to subprocess#check_call, but this
+    does two things:
+
+    * It does encoding these to bytes in Python 2.
+    * It does omitting `None` in *cmd.
+    
+    """
+    cmd = kwargs.get("args")
+    if cmd is None:
+        cmd = popenargs[0]
+    subprocess.check_call(
+        _filter_args(cmd), **kwargs)
+
+
+def check_stderroutput(*popenargs, **kwargs):
+    """
+    Unfortunately, ffmpeg and ffprobe throw out the information
+    we want into the standard error output, and subprocess.check_output
+    discards the standard error output. This function is obtained by
+    rewriting subprocess.check_output for standard error output.
+
+    And this does two things:
+
+    * It does encoding these to bytes in Python 2.
+    * It does omitting `None` in *cmd.
+    """
+    if 'stderr' in kwargs:
+        raise ValueError(
+            'stderr argument not allowed, it will be overridden.')
+    cmd = kwargs.get("args")
+    if cmd is None:
+        cmd = popenargs[0]
+    #
+    process = subprocess.Popen(
+        _filter_args(cmd),
+        stderr=subprocess.PIPE,
+        **kwargs)
+    stdout_output, stderr_output = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        raise subprocess.CalledProcessError(
+            retcode, cmd, output=stderr_output)
+    return stderr_output
+
+
+# ##################################
+#
+# higher-level APIs
+#
+def read_audio(audio_file):
+    """
+    Read file
+
+    INPUT: Audio file
+    OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
+    """
+    rate, data = scipy.io.wavfile.read(audio_file)  # Return the sample rate (in samples/sec) and data from a WAV file
+    return data, rate
+
+
+def get_media_info(filename):
+    """
+    return the information extracted by ffprobe.
+
+    for now, this function extracts only its duration.
+    """
+    err = check_stderroutput(["ffprobe", filename])
+    rgx = r"Duration: (\d{2}):(\d{2}):(\d{2}).(\d{2})"
+    tp = list(
+        map(int,
+            re.search(
+                rgx,
+                err.decode(sys.stdout.encoding)).group(1, 2, 3, 4)))
+    return {
+        "duration": tp[0] * 60 * 60 + tp[1] * 60 + tp[2] + tp[3] / 100.,
+        }
