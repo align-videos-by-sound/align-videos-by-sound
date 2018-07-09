cbc14373b7813ee3b15e8d9c0459a86beccf33c0
hhsprings <xwhhsprings@gmail.com>
2018-07-09 11:18:55 +0900

    For anything other than "pad_pre" and "trim_pre" you should calculate within "align" instead of "_align".

--- align_videos_by_soundtrack/align.py
+++ align_videos_by_soundtrack/align.py
@@ -208,17 +208,8 @@ class SyncDetector(object):
         # build result
         pad_pre = result - result.min()
         trim_pre = -(pad_pre - pad_pre.max())
-        infos = [self._get_media_info(fn) for fn in files]
-        orig_dur = np.array([inf["duration"] for inf in infos])
-        strms_info = [
-            (inf["streams"], inf["streams_summary"]) for inf in infos]
-        pad_post = list(
-            (pad_pre + orig_dur).max() - (pad_pre + orig_dur))
-        trim_post = list(
-            (orig_dur - trim_pre) - (orig_dur - trim_pre).min())
-
         #
-        return pad_pre, trim_pre, orig_dur, strms_info, pad_post, trim_post
+        return pad_pre, trim_pre
 
     def align(self, files, fft_bin_size=1024, overlap=0, box_height=512, box_width=43, samples_per_box=7,
               max_misalignment=0, known_delay_ge_map={}):
@@ -226,7 +217,7 @@ class SyncDetector(object):
         Find time delays between video files
         """
         # First, try finding delays roughly by passing low sample rate.
-        pad_pre, trim_pre, orig_dur, strms_info, pad_post, trim_post = self._align(
+        pad_pre, trim_pre = self._align(
             44100 // 4, files, fft_bin_size, overlap, box_height, box_width, samples_per_box,
             max_misalignment, known_delay_ge_map)
 
@@ -238,10 +229,19 @@ class SyncDetector(object):
         max_misalignment = 30
 
         # Finally, try finding delays precicely
-        pad_pre, trim_pre, orig_dur, strms_info, pad_post, trim_post = self._align(
+        pad_pre, trim_pre = self._align(
             self._sample_rate, files, fft_bin_size, overlap, box_height, box_width, samples_per_box,
             max_misalignment, known_delay_ge_map)
-
+        #
+        infos = [self._get_media_info(fn) for fn in files]
+        orig_dur = np.array([inf["duration"] for inf in infos])
+        strms_info = [
+            (inf["streams"], inf["streams_summary"]) for inf in infos]
+        pad_post = list(
+            (pad_pre + orig_dur).max() - (pad_pre + orig_dur))
+        trim_post = list(
+            (orig_dur - trim_pre) - (orig_dur - trim_pre).min())
+        #
         return [
             [
                 files[i],
@@ -339,7 +339,7 @@ It is possible to pass any media that ffmpeg can handle.',)
             max_misalignment=communicate.parse_time(args.max_misalignment),
             known_delay_ge_map=known_delay_ge_map)
     if args.json:
-        print(json.dumps({'edit_list': result}, indent=4))
+        print(json.dumps({'edit_list': result}, indent=4, sort_keys=True))
     else:
         report = []
         for i, path in enumerate(file_specs):
