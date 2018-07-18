13363ed3ee5500bb7b47bc03e7d438740f86ee38
hhsprings <xwhhsprings@gmail.com>
2018-07-18 17:27:29 +0900

    Suppose there are movies A and B. It is assumed that movie A is later than either of shot start and shot end of movie B. In such a case, we could not supplement the end of movie B with the end of movie A.
    "simple_compile_videos_by_sound_track" is based on the premise
    that "shot" starts the shot the earliest and finishes the shot
    at the latest, so it is hard to use with other patterns. This
    improvement on "concat_videos_by_sound_track" is a remedy for
    those patterns in "simple_compile_videos_by_sound_track".
    (For example, consider movie A as shot from the MC part of the
    concert but unmodering the encore, movie B omits the MC part
    and shoots the encore.)

--- align_videos_by_soundtrack/concat.py
+++ align_videos_by_soundtrack/concat.py
@@ -45,11 +45,12 @@ def _build(args):
     gaps = []
     #
     einf = []
+    start = 0
+    base_dur = 0
     with SyncDetector(
         params=args.summarizer_params,
         clear_cache=args.clear_cache) as sd:
 
-        start = 0
         upd = base not in known_delay_map
         for i in range(len(targets)):
             if upd and start:
@@ -62,11 +63,17 @@ def _build(args):
             res = sd.align(
                 [base, targets[i]],
                 known_delay_map=known_delay_map)
+            base_dur = res[0]["orig_duration"]
             gaps.append((start, res[0]["trim"] - start))
-            start = res[0]["trim"] + (res[1]["orig_duration"] - res[1]["trim"])
+            start = res[0]["trim"] + (
+                res[1]["orig_duration"] - res[1]["trim"])
             if i == 0:
                 einf.append(res[0])
             einf.append(res[1])
+    end_pad = False
+    if start < base_dur:
+        end_pad = True
+        gaps.append((start, base_dur - start))
     #
     qual = SyncDetector.summarize_stream_infos(einf)
     targets_have_video = any(qual["has_video"][1:])
@@ -76,8 +83,7 @@ def _build(args):
         h=qual["max_height"],
         fps=qual["max_fps"],
         sample_rate=qual["max_sample_rate"])
-    for i in range(len(targets)):
-        start, gap = gaps[i]
+    def _add_gap(start, gap):
         if gap > 0 and (
             not np.isclose(start, 0) or args.start_gap != "omit"):
 
@@ -86,7 +92,9 @@ def _build(args):
                     bld.add_video_gap(gap)
                 else:
                     fv = Filter()
-                    fv.add_filter("trim", "%.3f" % start, "%.3f" % (start + gap))
+                    fv.add_filter(
+                        "trim", "%.3f" % start,
+                        "%.3f" % (start + gap))
                     fv.add_filter("setpts", "PTS-STARTPTS")
                     bld.add_video_content(0, ",".join(
                             list(filter(None, [vf(0), fv.to_str()]))))
@@ -95,11 +103,15 @@ def _build(args):
                 bld.add_audio_gap(gap)
             else:
                 fa = Filter()
-                fa.add_filter("atrim", "%.3f" % start, "%.3f" % (start + gap))
+                fa.add_filter(
+                    "atrim", "%.3f" % start,
+                    "%.3f" % (start + gap))
                 fa.add_filter("asetpts", "PTS-STARTPTS")
                 bld.add_audio_content(0, ",".join(
                         list(filter(None, [af(0), fa.to_str()]))))
-
+    for i in range(len(targets)):
+        start, gap = gaps[i]
+        _add_gap(start, gap)
         if targets_have_video or qual["has_video"][0]:
             if qual["has_video"][i + 1]:
                 bld.add_video_content(i + 1, vf(i + 1))
@@ -107,6 +119,9 @@ def _build(args):
                 # wav, mp3, etc...
                 bld.add_video_gap(einf[i + 1]["orig_duration"])
         bld.add_audio_content(i + 1, af(i + 1))
+    if end_pad and args.end_gap == "pad":
+        start, gap = gaps[len(targets)]
+        _add_gap(start, gap)
     fc, vmap, amap = bld.build()
     return [base] + targets, fc, [vmap], [amap]
 
@@ -153,12 +168,22 @@ The default is "do not align" (`omit`) because it is not normally suitable for t
 purpose of `concatanate`. The default is "omit" if there are two or more media \
 specified as "splitted". If there is only one media specified as "splitted", \
 the default is "pad", assuming your goal is to fill in the leading gap.""")
+    parser.add_argument(
+        '--end_gap', choices=['omit', 'pad'],
+        help="""\
+Controling whether to align the end position with `base` or not. \
+The default is "do not align" (`omit`) because it is not normally suitable for the \
+purpose of `concatanate`. The default is "omit" if there are two or more media \
+specified as "splitted". If there is only one media specified as "splitted", \
+the default is "pad", assuming your goal is to fill in the leading gap.""")
     #####
     parser.editor_add_extra_ffargs_arguments()
     #####
     args = parser.parse_args(args[1:])
     if not args.start_gap:
         args.start_gap = "omit" if len(args.splitted) > 1 else "pad"
+    if not args.end_gap:
+        args.end_gap = "omit" if len(args.splitted) > 1 else "pad"
     cli_common.logger_config()
 
     files, fc, vmap, amap = _build(args)
